be8f57fcb978f5266b3f138a40232a44d2383a8c
José Alberto Flores <josefloressv@gmail.com>
2023-03-06 05:48:37 +0000

    Customize cluster

--- setenv.sh
+++ setenv.sh
@@ -1,8 +1,8 @@
 #-------Set the environment variables"
-export MY_CLUSTER=jfk8s01                   # Customize your cluster name
-export MY_INSTANCE_TYPE=t3a.large           # Customize your favorite machine type
+export MY_CLUSTER=jfeks01                   # Customize your cluster name
+export MY_INSTANCE_TYPE=t3a.medium           # Customize your favorite machine type
 export MY_REGION=us-east-1                  # Customize your favorite region
 export MY_ZONE=us-east-1a                   # Customize your favorite zone
 export MY_K8S_VERSION=1.25                  # Customize your Kubernetes Version
-export MY_K8S_NODE_GROUP_NAME=josefloressv  # Customize your Kubernetes Node Group Name
+export MY_K8S_NODE_GROUP_NAME=jfdev         # Customize your Kubernetes Node Group Name
 source ~/.bashrc                            # et environment variables
